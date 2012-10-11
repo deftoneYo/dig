8c2ddc1e964749efc6066e29e91f18f650697cdb
impetro - csg <jelrod@gmail.com>
2012-10-11 17:54:15 -0400

    added bootstrap sandbox. messed around with primary solution over last few weeks

---
+++ project/Dig.Data.DomainClasses/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dig.Data.DomainClasses
+{
+    public class Location
+    {
+	    public int LocationId { get; set; }
+	    public string Name { get; set; }
+	    public string NickName { get; set; }
+	    public int AddressId { get; set; }
+    }
+}
---
+++ project/Dig.Data/Context.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+
+namespace Dig.Data
+{
+	class Context : DbContext
+	{
+		
+	}
+}
